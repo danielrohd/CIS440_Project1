a34c4be7599136639dfff6ea4a3a5d4541be73e2
Daniel Rohd <drohd@asu.edu>
2021-09-16 18:23:50 -0700

    Installed PyQt5 and other tools, and created a UI file

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # Main Script
+"""
+To use PyQt5:
+pip install PyQt5
+pip install pyqt5-tools
+
+To open the designer:
+qt5-tools designer
+"""
+
+
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+class Ui(QtWidgets.QMainWindow):
+    """Imports the UI file"""
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('AppUI.ui', self)
+        self.show()
+
+
+# Creates an instance of the UI and starts it
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
