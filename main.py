c6b5b4bc81a3bb06fde0df848211ed48099f84f7
Brianna Poole <briannapoole@Brianna-MacBook.local>
2021-09-25 19:55:41 -0700

    added functionality for user to log in

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ import mysql.connector
 import userclass
 import event_class
 
-
 from AppUI import Ui_MainWindow
 
 
-def get_user_from_database(username, password):
+def get_user_from_database(enteredUsername, enteredPassword):
     """Creates a user from the database"""
     cnx = mysql.connector.connect(user='fall2021group5', password='group5fall2021',
                                   host='107.180.1.16',
                                   database='cis440fall2021group5')
     cursor = cnx.cursor()
-    query = f"SELECT first_name, last_name, username, password, email FROM Users WHERE username='{username}' " \
-            f"and password='{password}' "
+    query = f"SELECT first_name, last_name, username, password, email FROM Users WHERE username='{enteredUsername}' " \
+            f"and password='{enteredPassword}' "
     cursor.execute(query)
 
     for first_name, last_name, username, password, email in cursor:
-        if first_name is None:
-            # this needs to be fixed to control for if account does not exist
-            cnx.close()
-            return 0
-        else:
+        if username == enteredUsername:
             temp_user = userclass.User(first_name, last_name, username, password, email)
             cnx.close()
             return temp_user
+
+        else:
+            cnx.close()
+            return -1
 
 
 def get_host_events_from_database(host):
@@ -62,10 +61,36 @@ class MainWindow:
 
         self.ui.stackedWidget.setCurrentWidget(self.ui.login1)
 
-        self.ui.loginbutton.clicked.connect(self.goEvent)
+        self.ui.loginbutton.clicked.connect(self.clickedLogin)
         self.ui.events_tab.clicked.connect(self.goEvent)
         self.ui.friends_tab.clicked.connect(self.goFriends)
         self.ui.noti_tab.clicked.connect(self.goNoti)
+
+    def clickedLogin(self):
+        self.main_win.show()
+
+        # get username and password from text boxes and compare to what is in the database
+        usernameEntered = self.ui.username.text()
+        passwordEntered = self.ui.password.text()
+        result = get_user_from_database(usernameEntered, passwordEntered)
+
+        if result is None:
+           
+            self.ui.username.setText("")
+            self.ui.password.setText("")
+
+            # need to add in error message below that will tell the user the password and username are wrong
+            # requires adding a widget
+
+        else:
+
+            # reset text boxes to be blank
+            self.ui.username.setText("")
+            self.ui.password.setText("")
+
+            # goes to event window once successfully logged in
+            # can go to a different window, just did that to show the user it was successful
+            self.goEvent()
 
     def show(self):
         self.main_win.show()
@@ -88,5 +113,5 @@ if __name__ == '__main__':
     main_win.show()
     sys.exit(app.exec_())
 
-user = get_user_from_database('danielrohd', 'password123')
-print(user.email)
+##user = get_user_from_database('danielrohd', 'password123')
+##print(user.email)
