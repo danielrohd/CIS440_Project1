f98b74671efb98b607fdba1d4aaa0f6199047e92
Daniel Rohd <drohd@asu.edu>
2021-09-27 16:07:45 -0700

    added "add_account_to_database" function

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def get_user_from_database(enteredUsername, enteredPassword):
             f"and password='{enteredPassword}' "
     cursor.execute(query)
 
-    for first_name, last_name, username, password, email in cursor:
-        if username == enteredUsername:
-            temp_user = userclass.User(first_name, last_name, username, password, email)
-            cnx.close()
-            return temp_user
-
-        else:
-            cnx.close()
-            return -1
+    if cursor.rowcount == 0:
+        cnx.close()
+        return -1
+    else:
+        for first_name, last_name, username, password, email in cursor:
+            if username == enteredUsername and password == enteredPassword:
+                temp_user = userclass.User(first_name, last_name, username, password, email)
+                cnx.close()
+                return temp_user
 
 
 def get_host_events_from_database(host):
@@ -37,7 +37,7 @@ def get_host_events_from_database(host):
                             database='cis440fall2021group5')
     cursor = cnx.cursor()
 
-    query = f"SELECT title, date, location, host FROM Events where host='{host}'"
+    query = f"SELECT title, date, location, host FROM Events WHERE host='{host}'"
     cursor.execute(query)
 
     for title, date, location, host in cursor:
@@ -51,6 +51,33 @@ def get_host_events_from_database(host):
             cnx.close()
 
     return event_list
+
+
+def add_account_to_database(f_name, l_name, username, pw, email):
+    """Function can be used to add a user to the database after an account has been created"""
+    cnx = connector.connect(user='fall2021group5', password='group5fall2021',
+                            host='107.180.1.16',
+                            database='cis440fall2021group5')
+    cursor = cnx.cursor(buffered=True)
+
+    check_account = f'SELECT username FROM Users WHERE username="{username}"'
+    cursor.execute(check_account)
+
+    length = cursor.rowcount
+    if length == 0:
+        add_query = f"INSERT INTO Users (username, password, first_name, last_name, email) VALUES" \
+                    f" ('{username}', '{pw}', '{f_name}', '{l_name}', '{email}')"
+        cursor.execute(add_query)
+
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        return 1
+    else:
+        # if this else executes, this means the account already exists
+        # add a pop-up here that lets the user know the name is taken
+        return 0
+
 
 
 class MainWindow:
@@ -119,11 +146,14 @@ class MainWindow:
 
 friends = []
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main_win = MainWindow()
-    main_win.show()
-    sys.exit(app.exec_())
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     main_win = MainWindow()
+#     main_win.show()
+#     sys.exit(app.exec_())
 
-##user = get_user_from_database('danielrohd', 'password123')
-##print(user.email)
+# user = get_user_from_database('testuser', 'password321')
+# print(user.email)
+
+add_account_to_database('test', 'user', 'testuser', 'password321', 'user@user.com')
+
